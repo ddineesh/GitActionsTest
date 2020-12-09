481b0662a1de4726cc0092436fb21a9e26668817
Dasaratharao <dinesh.dasaratharao@capgemini.com>
2020-12-08 21:10:26 -0600

    initial commit for Git Action test

---
+++ src/main/java/com/dinesh/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.dinesh;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hello World for the Git hub actions");
+	}
+
+}
